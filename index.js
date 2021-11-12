f89910bc5fd056ba2c068c5fb5fe1402c8f7d1a6
Leo Clarke <leojclarke@gmail.com>
2021-11-12 23:13:01 +0100

    Initial setup and installations

---
+++ index.js
@@ -0,0 +1,16 @@
+const transactions = [
+    {
+        id: "1",
+        date: "2007-08-31T16:47+00:00",
+        vendor: {
+            shortname: "REWE",
+            fullname: "REWE 116 Pobric HH-Erdkampsweg",
+        },
+        account: {
+            id: "1",
+            name: "Postbank",
+            logo: "images/postbank.png"
+        },
+        amount: -22.33,
+    }
+]
