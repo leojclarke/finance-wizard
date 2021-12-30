c89b9ff90938c3c163917f5f4706788c56d2bfe4
Leo Clarke <leojclarke@gmail.com>
2021-12-30 15:29:44 +0100

    fix styling issues and semantic html

--- src/Transactions.js
+++ src/Transactions.js
@@ -1,31 +1,41 @@
-import React from 'react'
-import './Transactions.css'
-import transactionData from './data'
+import React from "react";
+import "./Transactions.css";
+import transactionData from "./data";
 
 export default function Transactions() {
-
-    return (
-        <ul>
-          <li className="transaction-row"><h2>November</h2></li>
-            {transactionData.map((data, key) => {
-                return (
-                  <li className="transaction-row" key={key}>
-                    <div>
-                      <img 
-                        className="transaction-logo"
-                        src={data.payee.imgsrc}
-                        alt={data.payee.shortname}
-                        width="60px"
-                      />
-                    </div>
-                    <div className="transaction-description">
-                      <p className="transaction-payee">{data.payee.shortname}</p>
-                      <p>{data.date}</p>
-                    </div>
-                    <div className="transaction-amount">{new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(data.amount)}</div>
-                  </li>
-                );
-            })}
+  return (
+    <section>
+      <div className="transaction-section-header">
+        <h2 className="transaction-month-header">November</h2>
+      </div>
+      <div>
+        <ul className="transactions-list">
+          {transactionData.map((data, key) => {
+            return (
+              <li className="transaction-row" key={key}>
+                <div>
+                  <img
+                    className="transaction-logo"
+                    src={data.payee.imgsrc}
+                    alt={data.payee.shortname}
+                    width="60px"
+                  />
+                </div>
+                <div className="transaction-description">
+                  <p className="transaction-payee">{data.payee.shortname}</p>
+                  <p>{data.date}</p>
+                </div>
+                <div className="transaction-amount">
+                  {new Intl.NumberFormat("de-DE", {
+                    style: "currency",
+                    currency: "EUR",
+                  }).format(data.amount)}
+                </div>
+              </li>
+            );
+          })}
         </ul>
-    );
-};
+      </div>
+    </section>
+  );
+}
